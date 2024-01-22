f3e979b124773bdbf1145097c0a214d2bcda94da
Santiago Jimenez Leudo <42678383+sjleudo@users.noreply.github.com>
2024-01-22 01:17:10 -0800

    Add files via upload

--- Angle_Control.m
+++ Angle_Control.m
@@ -1,20 +1,22 @@
 function [theta] = Angle_Control(ti, MRobots, ORobots, z, tau, q, eta, MF, OF, NMA, NOA, dt)
-    % Missing to remove the opponents that have a timeout or are tagged
-    Oponnent_Positions = Positions_in_Matrix(MRobots.(z)(ti,1), ORobots, NOA, ti); %Matrix NOAx2 where the positions of the NOA opponents are the rows 
+    [Oponnent_Positions, Attackers_Positions] = Positions_in_Matrix(MRobots.(z)(ti,1), ORobots, NOA, ti); %Matrix NOAx2 where the positions of the NOA opponents are the rows 
     [Oponnent_Positions, active_opponents] = multiplyFirstElement(MRobots.(z)(ti,1), Oponnent_Positions);% Filter the robots that are in the same half
+    [Attackers_Positions, active_attackers] = multiplyFirstElement(MRobots.(z)(ti,1), Attackers_Positions);% Filter the robots that are in the same half
     % Multiply the position of any robot that is not in the same half by a
     % 100 so it is not taken as the closest opponent
     Closest_Opponent_index = Closest_Opponent(MRobots.(z)(ti,1:2),Oponnent_Positions)-1;
+    Closest_Attacker_index = Closest_Opponent(MRobots.(z)(ti,1:2),Attackers_Positions)-1;
     fn = fieldnames(ORobots); 
     cz = fn{1+4*(Closest_Opponent_index-1)}; % Closest oponent in 'z_czB' or 'z_czR' format
-    theta = sin(ti*dt*4);
+    caz = fn{1+4*(Closest_Attacker_index-1)}; % Closest oponent in 'z_czB' or 'z_czR' format
+    %theta = sin(ti*dt*4);
         if MRobots.(q)(ti) == 1 || (MRobots.(eta)(ti) == 1 && MRobots.(z)(ti,1)*MF(1)>= 0 )
             % Return to base controller R and B
             theta = calculateAngle(MRobots.(z)(ti,1),MRobots.(z)(ti,2),MF(1),MF(2));
         elseif  MRobots.(eta)(ti) == 1 && MRobots.(z)(ti,1)*MF(1)<= 0
             if active_opponents > 0
                 % Return the flag (Avoid being intercepted by closest
-                % opponent) R and B
+                % defender that protects the boundary) R and B
                 m_x = -(MRobots.(z)(ti,2)-ORobots.(cz)(ti,2))/(MRobots.(z)(ti,1)-ORobots.(cz)(ti,1));
                 n_x = 0.5*(norm(MRobots.(z)(ti,1:2)-OF)^2-norm(ORobots.(cz)(ti,1:2)-OF)^2)/(MRobots.(z)(ti,1)-ORobots.(cz)(ti,1));
                 yst = 40*sign(m_x);
@@ -24,24 +26,26 @@ function [theta] = Angle_Control(ti, MRobots, ORobots, z, tau, q, eta, MF, OF, N
                 % being intercepted)
                 theta = (1+ sign(MRobots.(z)(ti,1)))*pi/2;
             end
-        elseif (active_opponents > 0) && MRobots.(eta)(ti) == 0 && MRobots.(q)(ti) == 0  && MRobots.(z)(ti,1)*MF(1)> 0
-            if MRobots.mu(ti) == 1
-                % Defend the flag (Capture the closest attacker)
-                xst = 0.5*(norm(ORobots.(cz)(ti,1:2)-MF)^2-norm(MRobots.(z)(ti,1:2)-MF)^2)*(ORobots.(cz)(ti,1)-MRobots.(z)(ti,1))/(norm(ORobots.(cz)(ti,1:2)-MRobots.(z)(ti,1:2))^2);
-                yst = 0.5*(norm(ORobots.(cz)(ti,1:2)-MF)^2-norm(MRobots.(z)(ti,1:2)-MF)^2)*(ORobots.(cz)(ti,2)-MRobots.(z)(ti,2))/(norm(ORobots.(cz)(ti,1:2)-MRobots.(z)(ti,1:2))^2);
-               theta = acos((xst-(MRobots.(z)(ti,1)-MF(1)))/norm([xst,yst]-(MRobots.(z)(ti,1:2)-MF)));
-            else 
+        elseif  active_attackers+ active_opponents> 0  && MRobots.(eta)(ti) == 0 && MRobots.(q)(ti) == 0 && MRobots.(tau)(ti) > 0 && MRobots.(z)(ti,1)*MF(1)> 0
+            if MRobots.mu(ti) == 1 && active_attackers > 0
+                % Defend the flag (Capture the closest attacker that aims to capture the flag)
+                xst = 0.5*(norm(ORobots.(caz)(ti,1:2)-MF)^2-norm(MRobots.(z)(ti,1:2)-MF)^2)*(ORobots.(caz)(ti,1)-MRobots.(z)(ti,1))/(norm(ORobots.(caz)(ti,1:2)-MRobots.(z)(ti,1:2))^2);
+                yst = 0.5*(norm(ORobots.(caz)(ti,1:2)-MF)^2-norm(MRobots.(z)(ti,1:2)-MF)^2)*(ORobots.(caz)(ti,2)-MRobots.(z)(ti,2))/(norm(ORobots.(caz)(ti,1:2)-MRobots.(z)(ti,1:2))^2);
+               theta = acos((xst-MRobots.(z)(ti,1))/norm([xst,yst]-(MRobots.(z)(ti,1:2))));
+            elseif  MRobots.mu(ti) == 0 && active_opponents > 0 %Optional: Find a way of finding who has the flag and go after him!!!
                 % Defend the boundary when the flag has been captured
                 m_x = -(ORobots.(cz)(ti,2)-MRobots.(z)(ti,2))/(ORobots.(cz)(ti,1)-MRobots.(z)(ti,1));
                 n_x = 0.5*(norm(ORobots.(cz)(ti,1:2)-OF)^2-norm(MRobots.(z)(ti,1:2)-OF)^2)/(ORobots.(cz)(ti,1)-MRobots.(z)(ti,1));
                 yst = 40*sign(m_x);
                 theta = (MF(1)<0)*pi+sign(MF(1))*acos((m_x*yst+n_x-(MRobots.(z)(ti,1)-OF(1)))/norm([m_x*yst+n_x,yst]-(MRobots.(z)(ti,1:2)-OF)));
+            else
+                theta = sin(ti*dt*4); % TO CHECK, because it only works for team blue
             end    
         elseif matches(z,'z_2B') && ORobots.mu(ti) == 1 
             if MRobots.(z)(ti,1)*MF(1)>0 
                 % Cross to the opponent's half plane
                 theta = (1+ sign(MRobots.(z)(ti,1)))*pi/2;
-            elseif MRobots.(z)(ti,1)*MF(1)<0
+            else%if MRobots.(z)(ti,1)*MF(1)<0
                 % Capture the flag of Red Team
                 theta = calculateAngle(MRobots.(z)(ti,1),MRobots.(z)(ti,2),OF(1),OF(2));
             end
--- Positions_in_Matrix.m
+++ Positions_in_Matrix.m
@@ -1,9 +1,10 @@
-function Matrix = Positions_in_Matrix(MRxp, Robots, NA,ti)
+function [Boundary_defenders_pos, attackers_pos] = Positions_in_Matrix(MRxp, Robots, NA,ti)
 % Rxp is the x position of my robot
 % Robots is the opponent team
 % Check team    
     fn = fieldnames(Robots);
-    Matrix = []; % We append here the positions of the opponent robots
+    Boundary_defenders_pos = []; % We append here the positions of the opponent robots
+    attackers_pos = [];
     % If the robot is tagged or has lost tagging ability, it position value
     % is stored as in the opposite half so it is not accounted as an active
     % opponent
@@ -11,6 +12,8 @@ function Matrix = Positions_in_Matrix(MRxp, Robots, NA,ti)
         z = fn{1+4*(m-1)};
         tau = fn{2+4*(m-1)};
         q = fn{3+4*(m-1)};
-        Matrix = [Matrix; -sign(MRxp)*100*((norm(MRxp)<norm(Robots.(z)(ti,1)))*(sign(MRxp*Robots.(z)(ti,1))>0)+...
+        Boundary_defenders_pos = [Boundary_defenders_pos; -sign(MRxp)*100*((norm(MRxp)>norm(Robots.(z)(ti,1)))*(sign(MRxp*Robots.(z)(ti,1))>0)+...
+            (Robots.(tau)(ti)<0)+Robots.(q)(ti))+Robots.(z)(ti,1:2)];
+        attackers_pos = [attackers_pos; -sign(MRxp)*100*((norm(MRxp)<norm(Robots.(z)(ti,1)))*(sign(MRxp*Robots.(z)(ti,1))>0)+...
             (Robots.(tau)(ti)<0)+Robots.(q)(ti))+Robots.(z)(ti,1:2)];
     end
